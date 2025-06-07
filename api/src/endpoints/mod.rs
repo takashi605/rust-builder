06b3b88611b8057673780aa020d12a5db2a9d675
takashi605 <tetocir@gmail.com>
2025-06-07 15:05:39 +0900

    テスト用の hello エンドポイントを削除

--- api/src/endpoints/mod.rs
+++ api/src/endpoints/mod.rs
@@ -4,13 +4,5 @@ use actix_web::{web, Scope};
 
 pub fn root_scope() -> Scope {
     web::scope("")
-        .service(hello)
         .service(user::get_users)
-}
-
-use actix_web::{get, HttpResponse, Responder};
-
-#[get("/")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("hello, world!!")
 }
