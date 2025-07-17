f410dcea8b9b3cf015572fc59acd48702592effb
takashi605 <tetocir@gmail.com>
2025-07-18 08:36:46 +0900

    MysqlQueryBuilder に select メソッド及び from メソッドを実装した #3

--- api/src/query_builder/builder/mysql/mod.rs
+++ api/src/query_builder/builder/mysql/mod.rs
@@ -10,18 +10,32 @@ impl MysqlQueryBuilder {
     }
 
     pub fn build(mut self) -> String {
-        self.query = String::from("SELECT id, name, email FROM users");
         self.query
+    }
+
+    pub fn select(mut self, columns: Vec<&str>) -> Self {
+        let columns_str = columns.join(", ");
+        self.query = format!("SELECT {}", columns_str);
+        self
+    }
+
+    pub fn from(mut self, table: &str) -> Self {
+        self.query = format!("{} FROM {}", self.query, table);
+        self
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
 
     #[test]
     fn test_build_select_users_query() {
         let query = MysqlQueryBuilder::new()
+            .select(vec!["id", "name", "email"])
+            .from("users")
             .build();
         assert_eq!(query, "SELECT id, name, email FROM users");
     }
