8bd6d6b6ac30d1f70d1facf7d83f658229aa506c
takashi605 <tetocir@gmail.com>
2025-07-22 08:12:17 +0900

    SelectUsersDirector について、 QueryBuilder を所有するように変更 #3

--- api/src/query_builder/directors/user.rs
+++ api/src/query_builder/directors/user.rs
@@ -1,10 +1,10 @@
-use crate::query_builder::builder::{mysql::MysqlQueryBuilder, QueryBuilder};
+use crate::query_builder::builder::QueryBuilder;
 
-pub struct SelectUsersDirector {
-    builder: MysqlQueryBuilder,
+pub struct SelectUsersDirector<B: QueryBuilder> {
+    builder: B,
 }
-impl SelectUsersDirector {
-    pub fn new(builder: MysqlQueryBuilder) -> Self {
+impl<B: QueryBuilder> SelectUsersDirector<B> {
+    pub fn new(builder: B) -> Self {
         SelectUsersDirector {
             builder,
         }
@@ -21,9 +21,10 @@ impl SelectUsersDirector {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::query_builder::builder::mysql::MysqlQueryBuilder;
 
     #[test]
-    fn test_select_users_director() {
+    fn test_select_users_director_with_mysql() {
         let builder = MysqlQueryBuilder::new();
         let director = SelectUsersDirector::new(builder);
         let query = director.build_query();
