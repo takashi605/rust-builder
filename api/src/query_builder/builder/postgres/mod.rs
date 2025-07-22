83f206dd2476c8cd7729f2a44458844177976ac1
takashi605 <tetocir@gmail.com>
2025-07-22 16:24:15 +0900

    PostgresQueryBuilder に upsert 文構築用のメソッドを追加した #4

--- api/src/query_builder/builder/postgres/mod.rs
+++ api/src/query_builder/builder/postgres/mod.rs
@@ -18,6 +18,9 @@ impl QueryBuilder for PostgresQueryBuilder {
         self.query
     }
 
+    /* 
+     * Select 用のメソッド
+     */
     fn select(mut self, columns: Vec<&str>) -> Self {
         let columns_str = columns.join(", ");
         self.query = format!("SELECT {}", columns_str);
@@ -26,6 +29,39 @@ impl QueryBuilder for PostgresQueryBuilder {
 
     fn from(mut self, table: &str) -> Self {
         self.query = format!("{} FROM {}", self.query, table);
+        self
+    }
+}
+
+impl PostgresQueryBuilder {
+    /* 
+     * Insert 用のメソッド
+     */
+    pub fn insert(mut self, table: &str) -> Self {
+        self.query = format!("INSERT INTO {}", table);
+        self
+    }
+
+    pub fn into(mut self, columns: Vec<&str>) -> Self {
+        let columns_str = columns.join(", ");
+        self.query = format!("{} ({})", self.query, columns_str);
+        self
+    }
+
+    pub fn values(mut self, values: Vec<&str>) -> Self {
+        let values_str = values.join(", ");
+        self.query = format!("{} VALUES ({})", self.query, values_str);
+        self
+    }
+
+    pub fn on_conflict(mut self, conflict_column: &str) -> Self {
+        self.query = format!("{} ON CONFLICT ({})", self.query, conflict_column);
+        self
+    }
+
+    pub fn do_update(mut self, update_columns: Vec<&str>) -> Self {
+        let updates_str = update_columns.join(", ");
+        self.query = format!("{} DO UPDATE SET {} = EXCLUDED.{}", self.query, updates_str, updates_str);
         self
     }
 }
@@ -44,6 +80,20 @@ mod tests {
             .build();
         assert_eq!(query, "SELECT id, name, email FROM users");
     }
+
+
+    #[test]
+    fn test_build_upsert_user_query() {
+        let query = PostgresQueryBuilder::new()
+            .insert("users")
+            .into(vec!["name", "email"])
+            .values(vec!["takashi", "takashi@example.com"])
+            .on_conflict("email")
+            .do_update(vec!["name"])
+            .build();
+        assert_eq!(query, "INSERT INTO users (name, email) VALUES (takashi, takashi@example.com) ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name");
+    }
+
 
     #[test]
     fn test_clone_query_builder() {
